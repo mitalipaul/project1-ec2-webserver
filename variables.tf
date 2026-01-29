dccf0337598498da0cb4753d717d2332f7b07704
Mitali Paul <paulmitali16@gmail.com>
2026-01-29 11:23:35 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "private_key_path" {
+
+  type = string
+
+  default = "./Neeharika_Terraform.pem"
+
+}
+
+variable "key_name" {
+
+  type = string
+
+  description = "key name"
+
+  default = "Neeharika_Terraform"
+
+}
+
+variable "region" {
+
+  type = string
+
+  default = "eu-west-1"
+
+  description = "Name of the region to create resource"
+
+}
+
+variable "network_address_space" {
+
+  default = "10.1.0.0/16"
+
+}
+
+variable "subnet_address_space" {
+
+  default = "10.1.0.0/24"
+
+}
